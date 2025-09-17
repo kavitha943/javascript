eb40f3cac0e4cae080b037be9871d14bc78c4edf
kavitha943 <kavithabarath0209@gmail.com>
2025-09-17 20:07:38 +0200

    todo-list js

---
+++ Day-9/9-todo-list.js
@@ -0,0 +1,29 @@
+/* using Javascript:
+ we can 1. save the data
+        2.Generate the HTML
+        Make it interactive */
+
+const todoList = [];
+renderTodoList();
+function renderTodoList() {
+
+
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`; //generating html
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');// this entire html element
+  //console.log(inputElement);
+  const name = inputElement.value;//this gets only value of input
+  //console.log(name);
+  todoList.push(name);
+  console.log(todoList);
+  //inputElement.value = '';
+  renderTodoList();
+}
