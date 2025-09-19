4e1555761bf21cc5275c66de3ef83aa9832783ad
kavitha943 <kavithabarath0209@gmail.com>
2025-09-19 19:48:27 +0200

    add script for todolist

---
+++ Day-10/js-scripts/10-todo-list.js
@@ -0,0 +1,57 @@
+/* Main Idea of  Javascript:
+ we can 1. save the data
+        2.Generate the HTML
+        3.Make it interactive */
+
+const todoList = [{
+  name:'make dinneer',
+  dueDate:'9-19-2025'
+},
+{
+  name:'wash dishes',
+  dueDate:'31-12-2025'
+}
+];
+renderTodoList();
+
+function renderTodoList() {
+
+
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    //const name=todoObject.name;
+    //const dueDate=todoObject.dueDate;
+    const {name, dueDate}=todoObject;
+    const html = `
+      
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+        todoList.splice(${i},1);
+        renderTodoList();"> 
+        Delete</button>
+      
+    `; //generating html
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');// this gets entire html element
+  //console.log(inputElement);
+  const name = inputElement.value;//this gets only value of input
+  //console.log(name);
+  const dateInputElement=document.querySelector('.js-due-date-input');
+  const dueDate= dateInputElement.value;
+  
+  todoList.push({//name: name,
+    //dueDate= dueDate,
+    name,
+    dueDate
+  });
+  console.log(todoList);
+  inputElement.value = '';
+  renderTodoList();
+}
